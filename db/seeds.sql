26eacd923d5c231b218200172be0d13a10c784e2
Kevin Valente <kevinevalente@gmail.com>
2017-07-27 15:56:38 -0700

    Need handlebars html

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, ts)
+VALUES ('Cheeseburger', false, CURRENT_TIMESTAMP),
+       ('Hamburger', false, CURRENT_TIMESTAMP),
+       ('Bacon Cheeseburger', false, CURRENT_TIMESTAMP);
